2e59588ccb512d4657988af18bec8e9fa62785b6
Akshay Kumar <88489895+imakshay13@users.noreply.github.com>
2022-01-20 13:16:41 +0530

    Update cryptoNewsApi.js

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const cryptoNewsHeaders = {
   "x-bingapis-sdk": "true",
   "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
-  "x-rapidapi-key": "0b2fdcf98dmsh77695007148988fp113be1jsn60658ed66812",
+  "x-rapidapi-key": "",
 };
 const baseUrl = "https://bing-news-search1.p.rapidapi.com";
 
